b5ef517eb21ab179cc41a86e19acba89bfb251ea
Arkhitech <arkhitech@Taimour-Mac-Book-Pro.local>
2022-07-19 11:00:23 +0500

    sessions branch changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,17 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.new
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            flash[:notice] = "Logged in successfully"
+            redirect_to user
+        else
+            flash.now[:alert] = "There was something wrong with your login details"
+            render :new
+        end
+
+
+
         render :new
 
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "user#index"
 
   get 'signup', to: 'sessions#new'
   get 'login', to: 'sessions#create'
