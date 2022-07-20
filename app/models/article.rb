e0c615158a9463212bb1d01c568af5b31a32a93d
Arkhitech <arkhitech@Taimour-Mac-Book-Pro.local>
2022-07-20 17:37:42 +0500

    many to many association formed

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
 
 
    belongs_to :user
+
+   has_many :article_categories
+   has_many :categories, through: :article_categories
   
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
